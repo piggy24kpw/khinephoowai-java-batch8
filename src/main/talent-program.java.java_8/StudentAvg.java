46fadbf5c5d012fec529f02b30bdf6a2520d42aa
piggy24kpw <khinephoowai2411@gmail.com>
2025-07-31 12:57:53 +0630

    initial commit

--- src/main/talent-program.java.java_8/StudentAvg.java
+++ src/main/talent-program.java.java_8/StudentAvg.java
@@ -4,28 +4,28 @@ public class StudentAvg {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
 
-        double total = 0;
-
         String ans;
         do {
             System.out.println("Enter the number of students: ");
             int total_student = input.nextInt();
 
             for (int i = 1; i <= total_student; i++) {
-
+                double total = 0;
                 System.out.println("Enter 3 grades for student " + i + ":");
                 for (int j = 1; j <= 3; j++) {
-                    System.out.print("Student " + j + ": ");
+                    System.out.print("Grade " + j + ": ");
                     double grade = input.nextDouble();
                     total += grade;
                 }
+
                 double avg = total / 3;
                 System.out.println("Average for student " + i + " is: " + avg);
             }
-            System.out.println("Do you want to continue");
+            input.nextLine();
+            System.out.println("Do you want to continue?(Yes/No)");
             ans = input.nextLine();
-        } while (ans == "Yes");
-
+        } while (ans.equalsIgnoreCase("Yes"));
+        input.close();
 
     }
 }
